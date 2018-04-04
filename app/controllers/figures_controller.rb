18eb6a7d9acbfe8c2c1f4c98f29649cbd274e97f
github username <github email address>
2018-04-04 17:51:27 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,9 @@
+require 'pry'
+
 class FiguresController < ApplicationController
 
-    get '/figures/' do
-        @figure = Figure.all
+    get '/figures' do
+        @figures = Figure.all
         erb :'/figures/index'
     end
         
@@ -9,10 +11,34 @@ class FiguresController < ApplicationController
         erb :'/figures/new'
     end
 
-    post '/figures/' do
-        @figure = Figure.create(params(:name))
-        @figure.save
-        erb :'figures/show'
+    get '/figures/:id' do
+        @figure = Figure.find_or_create_by(params(:id))
+        erb :'/figures/show'
     end
 
+    post '/figures' do
+        @figure = Figure.create(:name => params["name"])
+        # ??? why figure and not name in solution?
+        if !params["landmark"]["name"].empty?
+            @figure.landmarks << Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"])
+        end
+        if !params["title"]["name"].empty?
+            @figure.titles << Title.create(:name => params["title"]["name"])
+        end
+
+        # @figure.landmark = Landmark.find_or_create_by(:name => params['landmark']['name'])
+        # @figure.title = Title.find_or_create_by(:name => params['title']['name'])
+        # binding.pry
+        # ??? why not this in solution?
+
+       
+        # @figure.landmark_ids = params[:landmarks]
+        # ??? why in other labs and not this one acc. to solution?
+        # ??? how does params[:landmarks] work?
+        # @figure.title_ids = params[:titles]
+        @figure.save          
+        redirect "/figures/#{@figure.id}"
+    end
+
+    
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,34 @@
 class LandmarksController < ApplicationController
 
+    get '/landmarks' do
+        @landmarks = Landmark.all
+        erb :'/landmarks/index'    
+    end
+
+    get '/landmarks/new' do
+        erb :'/landmarks/new'
+    end
+
+    get '/landmarks/:id' do
+        @landmark = Landmark.find(params[:id])
+        erb :'/landmarks/:id/show'
+    end
+
+    get '/landmarks/:id/edit' do
+        @landmark = Landmark.find(params[:id])
+        erb :'/landmarks/:id/show'
+    end
+
+    post '/landmarks' do
+        @landmark = Landmark.create(:name => params['landmarks']['name'])
+        @landmark.figure = Figure.find_or_create_by(:name => params['figures']['name'])
+        @landmark.title = Title.find_or_create_by(:title => params['titles']['name'])
+        @landmark.figure_ids = params[:figures]
+        @landmark.title_ids = params[:titles]
+        @landmark.save          
+        redirect "/landmarks/#{@landmark.id}"
+        # ??? What's going on here with params[:figures]?
+        # ??? Where does figure_ids come from?
+    end
+
 end
