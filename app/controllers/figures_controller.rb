fb21fc88e1b74754b8ce7765a205a8b71de99931
github username <github email address>
2018-04-07 12:53:08 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -17,34 +17,35 @@ class FiguresController < ApplicationController
     end
 
     get '/figures/:id/edit' do 
+        # Where does the human user click on an edit button?
         @figure = Figure.find(params[:id])
         erb :'figures/edit'
     end
 
     post '/figures' do
         # ??? Why does this show the root url /figures ... is it supposed to go back to the index page? 
-        @figure = Figure.create(params["figure"])
+        @figure = Figure.create(params[:figure])
 
-            if !params["title"]["name"].empty?
-                @figure.titles << Title.create(params["title"])
+            if !params[:title][:name].empty?
+                @figure.titles << Title.create(params[:name])
             end
             
-            if !params["landmark"]["name"].empty?
-                @figure.landmarks << Landmark.create(params["landmark"])
+            if !params[:landmark][:name].empty?
+                @figure.landmarks << Landmark.create(params[:landmark])
             end
         
         @figure.save     
         redirect "/figures/#{@figure.id}"
     end
 
-    patch '/figures/:id' do
-        @figure = Figure.find(params["id"])
-        @figure.update(params["figure"]["name"])
+    post '/figures/:id' do
+        @figure = Figure.find(params[:id])
+        @figure.update(params[:figure][:name])
         @figure.save
-        @figure.landmark = Landmark.find(params["id"])
-        @figure.landmark.update(params["landmark"]["name"])
+        @figure.landmark = Landmark.find(params[:id])
+        @figure.landmark.update(params[:landmark][:name])
         @figure.landmark.save
-        @figure.title.update(params["title"]["name"])
+        @figure.title.update(params[:title][:name])
         @figure.landmark.save
         redirect "/figures/#{@figure.id}"
     end
--- spec/controllers/figures_controller_spec.rb
+++ spec/controllers/figures_controller_spec.rb
@@ -88,6 +88,7 @@ describe FiguresController do
   it "allows you to see a single Figure" do
     @figure = Figure.first
     get "/figures/#{@figure.id}"
+    # ??? Why not just "/figures/id"?
     expect(last_response.status).to eq(200)
     expect(last_response.body).to include("#{@figure.name}")
   end
