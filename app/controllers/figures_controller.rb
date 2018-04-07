cc941e8411eabb5c0dd3f9d0ad04ddcd62f2e1e8
github username <github email address>
2018-04-07 14:52:53 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -38,15 +38,28 @@ class FiguresController < ApplicationController
         redirect "/figures/#{@figure.id}"
     end
 
-    post '/figures/:id' do
+    patch '/figures/:id' do
+        # binding.pry
         @figure = Figure.find(params[:id])
-        @figure.update(params[:figure][:name])
+        @figure.update(params[:figure])
         @figure.save
         @figure.landmark = Landmark.find(params[:id])
-        @figure.landmark.update(params[:landmark][:name])
+        @figure.landmark.update(params[:landmark])
         @figure.landmark.save
-        @figure.title.update(params[:title][:name])
+        @figure.title.update(params[:title])
         @figure.landmark.save
+
+        # if !params[:figure][:name].empty?
+        #     @figure = Figure.create(params[:figure])
+        # end
+        
+        if !params[:title][:name].empty?
+            @figure.titles << Title.create(params[:name])
+        end
+        
+        if !params[:landmark][:name].empty?
+            @figure.landmarks << Landmark.create(params[:landmark])
+        end
         redirect "/figures/#{@figure.id}"
     end
 end
